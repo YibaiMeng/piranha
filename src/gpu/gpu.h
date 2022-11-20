80bbdac739533f93995cad23e73bf88854113a36
Yibai Meng <mengyibai@berkeley.edu>
2022-11-20 00:55:38 -0800

    feat: add CUDA_CHECK macro

--- src/gpu/gpu.h
+++ src/gpu/gpu.h
@@ -10,3 +10,11 @@
     }                                                                                              \
 }
 
+#define CUDA_CHECK(err)                                                                            \
+    do {                                                                                           \
+        cudaError_t err_ = (err);                                                                  \
+        if (err_ != cudaSuccess) {                                                                 \
+            std::printf("CUDA error %d at %s:%d\n", err_, __FILE__, __LINE__);                     \
+            throw std::runtime_error("CUDA error");                                                \
+        }                                                                                          \
+    } while (0)
